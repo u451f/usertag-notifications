160122d7a6a29bda582b651260b5d6b0b4a133e3
u <u@451f.org>
2015-02-23 12:54:57 +0100

    comment cleanup

--- udd.py
+++ udd.py
@@ -8,7 +8,7 @@
 # and https://wiki.debian.org/UltimateDebianDatabase
 #####################################################
 # Copyright 2015 u <u@451f.org>
-# Released under the GPLv3
+# Released under GPLv3
 # Made during rd9 of the GNOME Outreach program
 #####################################################
 
@@ -22,19 +22,23 @@ receiver = team_email_address
 smtp_server = "localhost"
 
 # connect to UDD
+# @params: none
+# @returns: db cursor
 def udd_connect():
     import psycopg2
     conn = psycopg2.connect("service=udd")
     cursor = conn.cursor()
     return cursor
 
-# select all usertagged bugs for our user
+# select all usertagged bugs for our user. query the bug title at the same time. construct a bug list
+# @params: str(Debian BTS maintainer / team email address)
+# @returns: list of dictionaries
 def get_bug_list(team_email_address):
     cursor = udd_connect()
     cursor.execute("SELECT bugs_usertags.id, bugs_usertags.tag, title FROM bugs_usertags JOIN bugs ON bugs_usertags.id = bugs.id WHERE email='%s' ORDER BY id" % team_email_address)
     items = cursor.fetchall()
 
-    # construct the list using a dictionary so we can retrieve the keys later
+    # construct the bug list: a list of dictionaries for each bug.
     buglist = []
     for item in items:
         bug = {'id': item[0], 'tag': item[1], 'title': item[2]}
@@ -42,12 +46,13 @@ def get_bug_list(team_email_address):
 
     return buglist
 
-# take a list of bugnumbers and usertags and save them to a file
+# use pickle to save the list of bugs to a file
+# @params: str(state_filename), data = list of dictionaries
+# @returns: True on success, False on failure
 def save_statefile(state_filename, data):
     import pickle
     try:
         f = open(state_filename, 'wb')
-        # Pickle data dictionary using protocol 0.
         pickle.dump(data, f)
         f.close()
     except IOError as e:
@@ -56,22 +61,27 @@ def save_statefile(state_filename, data):
 
     return True
 
-# load old data string and convert it to a dictionary
-# @returns dictionary with old state
+# use pickle to read the list of bugs from a file
+# for debugging the data, use pprint
+# @params: str(state_filename)
+# @returns: list of dictionaries, False on read failure
 def read_statefile(state_filename):
-    import pprint, pickle
+    # import pprint
+    import pickle
     try:
         f = open(state_filename, 'rb')
-        old = pickle.load(f)
+        data = pickle.load(f)
         f.close()
     except IOError as e:
         # send_error_mail("Could not read state file.")
         return False
 
     # pprint.pprint(old)
-    return old
+    return data
 
 # compare two lists of dictionaries
+# @params: two lists of dictionaries ({'id': '123', 'tag': 'the_tag', 'title': 'The Title'})
+# @returns: two lists of dictionaries
 def compare_state(old_state_data, new_state_data):
     deleted_usertags = []
     added_usertags = []
@@ -93,21 +103,26 @@ def compare_state(old_state_data, new_state_data):
     return added_usertags, deleted_usertags
 
 # send one email per bug to the team
-# @diff = added | deleted
-def send_team_notification(sender, receiver, bug_list, diff, bdo_url, usertag_url):
-    notifications = []
-    # construct notification text for each bug
+# @params: operation = str "added" | "deleted"
+#          sender = email address
+#          receiver = email address
+#          bug_list = list of dictionaries ({'id': '123', 'tag': 'the_tag', 'title': 'The Title'})
+#          bdo_url = str
+#          usertag_url = str
+def send_team_notification(sender, receiver, bug_list, operation, bdo_url, usertag_url):
+    # construct notification text for each bug and send email
     for bug in bug_list:
-    	# print "usertag '%s' %s on bug #%s: %s" % (bug['tag'], diff, bug['id'], bug['title'])
-    	notification_subject = "usertag '%s' %s on bug #%s: %s" % (bug['tag'], diff, bug['id'], bug['title'])
-    	notification_msg = "%s%s\n\nSee all usertags: %s" % (bdo_url, bug['id'], usertag_url)
-    	notifications.append([notification_subject, notification_msg])
-    # send team notification
-    if notifications:
-    	for notification in notifications:
-    		send_mail(sender, receiver, notification[0], notification[1])
+    	# print "usertag '%s' %s on bug #%s: %s" % (bug['tag'], operation, bug['id'], bug['title'])
+        notification_subject = "usertag '%s' %s on bug #%s: %s" % (bug['tag'], operation, bug['id'], bug['title'])
+        notification_msg = "%s%s\n\nSee all usertags: %s" % (bdo_url, bug['id'], usertag_url)
+        send_mail(sender, receiver, notification_subject, notification_msg)
 
 # send an email.
+# @params: str(sender) = email address
+#          str(receiver) = email address
+#          str(subject) = email body
+#          str(text) = email test
+# @returns: void
 def send_mail(sender, receiver, subject, text):
     # Import smtplib for the actual sending function and mail modules
     import smtplib
@@ -129,6 +144,10 @@ def send_mail(sender, receiver, subject, text):
     s.sendmail(receiver, [sender], msg.as_string())
     s.quit()
 
+# send error email
+# @params: str(msg) = message text
+# @global: str(sender), str(receiver)
+# @returns: void
 def send_error_mail(msg):
     global sender, receiver
     subject = "udd.py caused an error"
@@ -140,14 +159,12 @@ current_buglist = get_bug_list(team_email_address)
 old_buglist = read_statefile(state_filename)
 
 # attempt to save the file if there is not yet an old_buglist
-if current_buglist and not old_buglist:
+if not old_buglist:
     save_statefile(state_filename, current_buglist)
 
+# retrieve usertag diff, then send notifications to the team and re-save the current state
 if current_buglist and old_buglist:
-    # retrieve usertag diff
     added_usertags, deleted_usertags = compare_state(old_buglist, current_buglist)
-    # send notification to the team
     send_team_notification(sender, receiver, added_usertags, "added", bdo_url, usertag_url)
     send_team_notification(sender, receiver, deleted_usertags, "deleted", bdo_url, usertag_url)
-    # save the current state
     save_statefile(state_filename, current_buglist)
